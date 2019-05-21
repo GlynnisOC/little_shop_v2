5e899583a8db13d72093db24a80345ed9428ba8a
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-05-21 10:45:06 -0600

    Added item spec and relationships

--- spec/features/items/user_sees_an_item_index_spec.rb
+++ spec/features/items/user_sees_an_item_index_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'As a user on the items index page' do
   describe 'I see all enabled items in the database' do
 		before :each do 
 			@merchant_1 = User.create!(email: "Bob@bob.bob", password_digest: "password", role: 1, active: true, name: "Bob Bob", address: "123 Shady Lane", city: "Boulda", state: "CO", zip: "80303")
+       @merchant_1.items.create!(name: "name", active: true, price: 2.20, description: "Buy things with your MONEY!", image: "fake url", inventory: 500)
 		end
 
 		it 'shows the name, thumbnail, merchant, inventory stock, and current price' do
