a613e911b846292d48efd29350adfd1ff1bf48d0
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-05-21 10:51:39 -0600

    Adds has many items assertation in user spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe Item, type: :model do
 	describe 'relationships' do
-		it {should belong_to user} 
+		it {should belong_to :user} 
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  #it 'relationships' do
-  #
-  #end	
+  it 'relationships' do
+		it {should have_many :items}  
+  end	
+	
 	describe 'validations' do
 		it {should validate_presence_of :email}
 		it {should validate_presence_of :password_digest}
