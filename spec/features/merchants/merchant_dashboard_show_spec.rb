f1522b0d169c4a574f81fd7113c94bbc6e5505be
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-05-22 14:55:58 -0600

    Adds merchant dashboard show spec test

---
+++ spec/features/merchants/merchant_dashboard_show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'As a registered merchant on the site' do
+	describe 'when I visit my dashboard' do
+		it 'shows my data and I cannot edit it' do 
+						@merchant = User.create!(email: "jimbob@bob.bob", password: "password", role: "merchant", name: "jimbob", address: "321 blastoff st", city: "Big Greasy Easy", state: "LA", zip: 38511) 
+			visit dashboard_path
+
+			within "#merchant-profile-info" do
+				expect(page).to have_content("Name: #{@merchant.name}")
+				expect(page).to have_content("Email: #{@merchant.email}")
+				expect(page).to have_content("Address: #{@merchant.address}")
+				expect(page).to have_content("City: #{@merchant.city}")
+				expect(page).to have_content("State: #{@merchant.state}")
+			end
+		end
+	end
+end
