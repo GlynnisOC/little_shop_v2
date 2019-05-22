7b2ab56d1df571fa90c570d5994a8aac4bc48b1d
James Cape <james.cape@gmail.com>
2019-05-22 14:44:54 -0600

    Add logout redirect and modify logout controller
    Co-Authored-By: Brian Plantico 
    <43261385+bplantico@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def logout
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'merchants#dashboard', as: 'dashboard'
   get '/merchants', to: 'merchants#index'
 
-  get '/logout', to: 'welcome#index'
+  get '/logout', to: 'application#logout'
 
   resources :items, only: [:index, :show]
 
