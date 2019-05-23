a32912a02cc394c80d2b1cc1e2df5de9a8422deb
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-05-22 19:51:42 -0600

    Added test for merchant dashboard items routes and links

--- spec/features/merchants/merchant_can_see_their_items_index_spec.rb
+++ spec/features/merchants/merchant_can_see_their_items_index_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 RSpec.describe 'As a registered merchant on my dashboard page' do
 	describe 'I see a link to view my own items' do
 		it 'takes me to /dashboard/items' do
+      merchant_1 = User.create!(email: "Bob@bob.bob", password: "password", role: 1, active: true, name: "Bob Bob", address: "123 Shady Lane", city: "Boulda", state: "CO", zip: "80303")
 
+			allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_1)
+
+			visit dashboard_path
+
+			click_link "View my Items"
+
+			expect(current_path).to eq('/dashboard/items')
 		end
 	end
 end
