9f5471c938dd7afbc4f04c01ae8b4ccb422176e8
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-05-22 19:47:10 -0600

    Re-arranged files and created new tests for merchant items index

---
+++ spec/features/merchants/merchant_can_see_all_their_items_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+#RSpec.describe 'As a registered merchant on my dashboard page' do
+#	describe 'I see a link to view my own items' do
+#		it 'takes me to /dashboard/items' do
+#
+#		end
+#	end
+#end
