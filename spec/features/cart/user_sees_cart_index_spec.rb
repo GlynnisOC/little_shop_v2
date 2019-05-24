0db99a4ee9e38a4e334db132a5ce4f8c1e90eca7
James Cape <james.cape@gmail.com>
2019-05-23 22:56:44 -0600

    Adds user show page with tests, but routes need to change from index to show

--- spec/features/cart/user_sees_cart_index_spec.rb
+++ spec/features/cart/user_sees_cart_index_spec.rb
@@ -2,7 +2,7 @@
 # When I have added items to my cart
 # And I visit my cart ("/cart")
 # - [x] I see all items I've added to my cart
-# - [ ] And I see a link to empty my cart
+# - [x] And I see a link to empty my cart
 # Each item in my cart shows the following information:
 # - [x] the name of the item
 # - [x] a very small thumbnail image of the item
@@ -21,9 +21,9 @@ RSpec.describe "as a user on the site" do
       merchant_1 = User.create!(email: "Rob@rob.rob", password: "password", role: 1, active: true, name: "Rob Rob", address: "123 Shady Lane", city: "Boulda", state: "CO", zip: "80303")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
-      active_item_1 = merchant_1.items.create!(name: "name", active: true, price: 2.20, description: "Buy things with your MONEY!", image: "https://images.unsplash.com/photo-1443557661966-8b4795a6f62c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", inventory: 500)
-      active_item_2 = merchant_1.items.create!(name: "name", active: true, price: 2.202, description: "Buy things with your MONEY!", image: "https://images.unsplash.com/photo-1443557661966-8b4795a6f62c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", inventory: 500)
-      active_item_3 = merchant_1.items.create!(name: "name", active: true, price: 2.20, description: "Buy things with your MONEY!", image: "https://images.unsplash.com/photo-1443557661966-8b4795a6f62c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", inventory: 500)
+      active_item_1 = merchant_1.items.create!(name: "name_1", active: true, price: 2.20, description: "Buy things with your LAND!", image: "https://images.unsplash.com/photo-1443557661966-8b4795a6f62c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", inventory: 400)
+      active_item_2 = merchant_1.items.create!(name: "name_2", active: true, price: 2.40, description: "Buy things with your MONEY!", image: "https://images.unsplash.com/photo-1443557661966-8b4795a6f62c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", inventory: 500)
+      active_item_3 = merchant_1.items.create!(name: "name_3", active: true, price: 3.50, description: "Buy things with your EMOTIONS!", image: "https://images.unsplash.com/photo-1443557661966-8b4795a6f62c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", inventory: 600)
 
       visit item_path(active_item_1.id)
 
@@ -52,10 +52,84 @@ RSpec.describe "as a user on the site" do
       expect(page).to have_content("Cart: 3")
 
       visit cart_path
+      expect(page).to have_content("Item Name: #{active_item_1.name}")
+      expect(page).to have_xpath("//img[contains(@src,'#{active_item_1.image}')]")
+      expect(page).to have_content("Merchant: #{merchant_1.name}")
+      expect(page).to have_content("Price: $#{active_item_1.price}0")
+      expect(page).to have_content("Desired Quantity: 2")
+      expect(page).to have_content("Subtotal Price: $4.40")
+
+      expect(page).to have_content("Item Name: #{active_item_2.name}")
+      expect(page).to have_xpath("//img[contains(@src,'#{active_item_2.image}')]")
+      expect(page).to have_content("Merchant: #{merchant_1.name}")
+      expect(page).to have_content("Price: $#{active_item_2.price}0")
+      expect(page).to have_content("Desired Quantity: 1")
+      expect(page).to have_content("Subtotal Price: $2.40")
+
+      expect(page).to have_content("Grand Total: $6.80")
 
       click_link "Empty Cart"
 
-      save_and_open_page
+      expect(page).to have_content("Cart: 0")
+    end
+  end
+end
+
+RSpec.describe "as a visitor on the site" do
+  describe "visitor added items to cart and visited /cart" do
+    it "shows visitor all the items added to the cart" do
+      merchant_1 = User.create!(email: "Rob@rob.rob", password: "password", role: 1, active: true, name: "Rob Rob", address: "123 Shady Lane", city: "Boulda", state: "CO", zip: "80303")
+
+      active_item_1 = merchant_1.items.create!(name: "name_1", active: true, price: 2.20, description: "Buy things with your LAND!", image: "https://images.unsplash.com/photo-1443557661966-8b4795a6f62c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", inventory: 400)
+      active_item_2 = merchant_1.items.create!(name: "name_2", active: true, price: 2.40, description: "Buy things with your MONEY!", image: "https://images.unsplash.com/photo-1443557661966-8b4795a6f62c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", inventory: 500)
+      active_item_3 = merchant_1.items.create!(name: "name_3", active: true, price: 3.50, description: "Buy things with your EMOTIONS!", image: "https://images.unsplash.com/photo-1443557661966-8b4795a6f62c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", inventory: 600)
+
+      visit item_path(active_item_1.id)
+
+      expect(page).to have_content("Cart: 0")
+
+      within("#visitor-nav-cart") do
+        click_button "Add to Cart"
+      end
+
+      visit item_path(active_item_1.id)
+
+      expect(page).to have_content("Cart: 1")
+
+      within("#visitor-nav-cart") do
+        click_button "Add to Cart"
+      end
+
+      expect(page).to have_content("Cart: 2")
+
+      visit item_path(active_item_2.id)
+
+      within("#visitor-nav-cart") do
+        click_button "Add to Cart"
+      end
+
+      expect(page).to have_content("Cart: 3")
+
+      visit cart_path
+      expect(page).to have_content("Item Name: #{active_item_1.name}")
+      expect(page).to have_xpath("//img[contains(@src,'#{active_item_1.image}')]")
+      expect(page).to have_content("Merchant: #{merchant_1.name}")
+      expect(page).to have_content("Price: $#{active_item_1.price}0")
+      expect(page).to have_content("Desired Quantity: 2")
+      expect(page).to have_content("Subtotal Price: $4.40")
+
+      expect(page).to have_content("Item Name: #{active_item_2.name}")
+      expect(page).to have_xpath("//img[contains(@src,'#{active_item_2.image}')]")
+      expect(page).to have_content("Merchant: #{merchant_1.name}")
+      expect(page).to have_content("Price: $#{active_item_2.price}0")
+      expect(page).to have_content("Desired Quantity: 1")
+      expect(page).to have_content("Subtotal Price: $2.40")
+
+      expect(page).to have_content("Grand Total: $6.80")
+
+      click_link "Empty Cart"
+
+      expect(page).to have_content("Cart: 0")
     end
   end
 end
