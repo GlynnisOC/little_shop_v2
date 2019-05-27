2e88cc15169dc2088e34a2de3c01841f74507b22
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-05-27 12:47:48 -0600

    Add status to admin dashboard view, but not yet sorting by status

--- spec/features/admin/admin_dashboard_spec.rb
+++ spec/features/admin/admin_dashboard_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe "when I log into the admin dashboard" do
 		@user_1 = create(:user)
     @order = create(:order)
     @order2 = create(:order)
+    @order3 = create(:order)
+    @order4 = create(:order)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin_1)
 	end
 
@@ -30,11 +32,17 @@ RSpec.describe "when I log into the admin dashboard" do
     expect(page).to have_content("#{@order.created_at}")
     expect(page).to have_content("#{@order2.created_at}")
   end
+
+  it "I can see the orders sorted by packaged, pending, shipped, and cancelled" do
+    @order.status = 0
+    @order2.status = 1
+    @order3.status = 2
+    @order4.status = 3
+    visit admin_dashboard_path
+
+    expect(page).to have_content("#{@order.status}")
+    expect(page).to have_content("#{@order2.status}")
+    expect(page).to have_content("#{@order3.status}")
+    expect(page).to have_content("#{@order4.status}")
+  end
 end
-#
-# Orders are sorted by "status" in this order:
-#
-# - packaged
-# - pending
-# - shipped
-# - cancelled
