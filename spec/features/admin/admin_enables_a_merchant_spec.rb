1765432c155788e240e0ce1103c2d42defd4a0ae
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-05-27 15:55:32 -0600

    Completes merchant enable spec

--- spec/features/admin/admin_enables_a_merchant_spec.rb
+++ spec/features/admin/admin_enables_a_merchant_spec.rb
@@ -1,9 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'As an admin on the merchant index ' do
-	describe 'when I click ' do
-		xit '' do
+	describe 'when I click the enable button for a disabled merchant' do
+		it 'enables the merchant account, posts a flash message, and allows the merchant to login' do
+			merchant = create(:merchant)	
+  	  admin = create(:admin) 
+			visit login_path
+  	  fill_in "Email", with: admin.email
+  	  fill_in "Password", with: admin.password
+  	  click_button("Login")
+			
+			visit admin_merchants_path
 
+			click_button("Enable")
+			expect(current_path).to eq(admin_merchants_path)
+			expect(page).to have_content("#{inactive_merchant.name} is now enabled")
+			click_link "Logout"
+
+			inactive_merchant.reload
+			visit login_path
+  	  fill_in "Email", with: inactive_merchant.email
+  	  fill_in "Password", with: inactive_merchant.password
+  	  click_button("Login")
+			expect(current_path).to eq(dashboard_path)	
 		end
 	end
 end
