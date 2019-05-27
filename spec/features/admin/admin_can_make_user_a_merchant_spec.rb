df74397643a682ffefd10669119fb2ea68078cc3
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-05-27 13:01:28 -0600

    Add updates from master branch.

--- spec/features/admin/admin_can_make_user_a_merchant_spec.rb
+++ spec/features/admin/admin_can_make_user_a_merchant_spec.rb
@@ -1,9 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe 'As an admin user on a user show page' do
+RSpec.describe 'As an admin user on an admin user show page' do
 	describe 'I see a link to upgrade that user to a merchant' do
-		xit '' do
+		before :each do 
+			@admin = create(:admin, email: "admin_1@gmail.com")
+			@user_1 = create(:user, email: "user_1@gmail.com")
+			@user_2 = create(:user, email: "user_2@gmail.com")
+			visit login_path
+			fill_in "Email", with: "admin_1@gmail.com"
+			fill_in "Password", with: "password"
+			click_button "Login"	
+		end
 
+		it 'upgrades and redirects to their show page where I see a flash message' do
+			visit admin_user_path(@user_1)
+			click_link("Upgrade to Merchant")
+
+			expect(current_path).to eq(admin_merchant_path(@user_1))	
+			expect(page).to have_content("#{@user_1.name} is now a merchant")			
+			visit logout_path
+		end
+
+	it 'the user is a merchant the next time they log in' do
+			visit admin_user_path(@user_2)
+			click_link("Upgrade to Merchant")
+
+			visit logout_path
+			visit login_path
+	
+			fill_in "Email", with: "user_2@gmail.com"
+			fill_in "Password", with: "password"
+			click_button "Login"	
+
+			expect(current_path).to eq(dashboard_path)
 		end
 	end
 end
