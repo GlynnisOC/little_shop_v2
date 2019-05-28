a3df076ee32a129b6e747b3ae3e773f3827622c5
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-05-28 14:54:23 -0600

    Needed to change branches so quick commit

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,6 @@ class Order < ApplicationRecord
 	has_many :order_items
 	has_many :items, through: :order_items
 
-	# belongs_to :user
-
 	enum status: [:pending, :packaged, :shipped, :cancelled]
 
 	def order_grand_total
