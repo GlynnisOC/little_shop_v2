b399128215490cee3d1657b5c0494ad115bd46e7
James Cape <james.cape@gmail.com>
2019-05-28 16:52:39 -0600

    Tests existence of button for correct orders

--- spec/features/admin/admin_marks_order_shipped_spec.rb
+++ spec/features/admin/admin_marks_order_shipped_spec.rb
@@ -1,8 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'as an admin user on my dashboard'  do
-	describe '' do
-		xit '' do
+	describe 'I see packaged orders ready to ship' do
+		describe 'and each order has a button to ship the order' do
+			before :each do
+				@admin_1 = create(:admin)
+				# @user_1 = create(:user)
+				allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin_1)
+
+				@order_1 = create(:order)
+				@order_2 = create(:packaged_order)
+				@order_3 = create(:shipped_order)
+				@order_4 = create(:cancelled_order)
+				visit admin_dashboard_path
+				# enum status: [:pending, :packaged, :shipped, :cancelled]
+			end
+
+			it 'button exists when order status is packaged_order' do
+				within "#admin-dashboard-order-#{@order_1.id}" do
+					expect(page).to_not have_button("Ship Order")
+				end
+
+				within "#admin-dashboard-order-#{@order_2.id}" do
+					expect(page).to have_button("Ship Order")
+				end
+
+				within "#admin-dashboard-order-#{@order_3.id}" do
+					expect(page).to_not have_button("Ship Order")
+				end
+
+				within "#admin-dashboard-order-#{@order_4.id}" do
+					expect(page).to_not have_button("Ship Order")
+				end
+			end
+
 
 		end
 	end
--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe OrderItem, type: :model do
 	describe 'relationships' do
 		it {should belong_to :item}
+		# it {should belong_to :order}
 	end
 end
