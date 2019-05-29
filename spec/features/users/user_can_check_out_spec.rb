d0e884c7a322c87e735a8283fb10452ca9ca2d99
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-05-28 19:21:18 -0600

    Finishes check out story 26

--- spec/features/users/user_can_check_out_spec.rb
+++ spec/features/users/user_can_check_out_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe 'as a registered user with items in my cart' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 			visit item_path(@item_1)
 			click_button "Add to Cart"
+			visit item_path(@item_1)
+			click_button "Add to Cart"
 			visit item_path(@item_2)
 			click_button "Add to Cart"
 			visit cart_path
@@ -18,9 +20,9 @@ RSpec.describe 'as a registered user with items in my cart' do
 		it 'creates a pending order, empties my cart, and redirects to profile/orders with a flash message' do
 			click_button("Check Out")
 			expect(current_path).to eq('/profile/orders')
-			expect(Order.all).to eq(1)
+			expect(Order.all.count).to eq(1)
 			expect(page).to have_content("Your order has been placed")
-		save_and_open_page		
+			expect(page).to have_content("Cart: 0")
 		end
 	end
 end
