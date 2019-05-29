18b5574ac80a53d793be18f68ca25fa65fe9d5c2
Brian Plantico <bplantico@gmail.com>
2019-05-29 13:22:39 -0600

    Sad path testing
    Co-Authored-By: Rob Stringer <41843577+Mycobee@users.noreply.github.com>

--- spec/features/merchants/merchant_can_not_fulfill_orders_without_inventory_spec.rb
+++ spec/features/merchants/merchant_can_not_fulfill_orders_without_inventory_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'As a merchant on my dashboard order show page' do
 			@order = create(:order, user: @user)
 			@item_1 = create(:item, user: @merchant, inventory: 5)
 			@item_2 = create(:item, user: @merchant, inventory: 1)
-			@order_item_1 = create(:order_item, item: @item_1, order: @order, quantity: 3)
+			@order_item_1 = create(:order_item, item: @item_1, order: @order, quantity: 5)
 			@order_item_2 = create(:order_item, item: @item_2, order: @order, quantity: 3)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
